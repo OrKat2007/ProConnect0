e07b0c0d4a05f99d69d809d74945c183cb2737f7
Orkat2007 <orkat2007@gmail.com>
2025-01-19 14:06:39 +0200

    usersetting profile not compltiinting

--- app/src/main/java/com/example/proconnect/user_settings.java
+++ app/src/main/java/com/example/proconnect/user_settings.java
@@ -1,28 +1,109 @@
 package com.example.proconnect;
 
+import static com.google.api.ResourceProto.resource;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class user_settings extends Fragment {
+
+    TextView username;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_user_settings, container, false);
-        ImageView profileImage = view.findViewById(R.id.profileImage);
+
+        ImageButton profileImage = view.findViewById(R.id.profileImage);
         Glide.with(this)
+                .asBitmap()
                 .load(R.drawable.home)
                 .apply(RequestOptions.circleCropTransform())
-                .into(profileImage);
+                .into(new CustomTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        profileImage.setImageBitmap(resource);
+                    }
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+                        profileImage.setImageDrawable(placeholder);
+                    }
+                });
+        username = view.findViewById(R.id.userName);
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (currentUser != null && currentUser.getDisplayName() != null) {
+            username.setText(currentUser.getDisplayName());
+        } else {
+            username.setText("Unknown User");
+        }
 
         return view;
     }
+    private static final String IMAGE_DIRECTORY = "/demonuts";
+    private int GALLERY = 1, CAMERA = 2;
+    final int REQUEST_CODE_GALLERY = 999;
+    private void showPictureDialog(){
+        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(getContext()); // Use getContext() instead of 'this'
+        pictureDialog.setTitle("נא לבחור מאיפה להוסיף תמונה:");
+        String[] pictureDialogItems = {
+                "מהגלריה",
+                "מהמצלמה" };
+        pictureDialog.setItems(pictureDialogItems,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                choosePhotoFromGallary();
+                                break;
+                            case 1:
+                                takePhotoFromCamera();
+                                break;
+                        }
+                    }
+                });
+        pictureDialog.show();
+    }
+
+    public void choosePhotoFromGallary() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+
+        startActivityForResult(galleryIntent, GALLERY);
+    }
+
+    private void takePhotoFromCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA);
+    }
+
+
+
+
+
 }
