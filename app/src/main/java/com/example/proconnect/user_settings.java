cf7021507ff80d1a93d180713dc35d0fbbafb745
Orkat2007 <orkat2007@gmail.com>
2025-01-19 14:16:55 +0200

    usersetting profile not compltiinting

--- app/src/main/java/com/example/proconnect/user_settings.java
+++ app/src/main/java/com/example/proconnect/user_settings.java
@@ -1,60 +1,55 @@
 package com.example.proconnect;
 
-import static com.google.api.ResourceProto.resource;
-
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
-import com.bumptech.glide.request.target.CustomTarget;
-import com.bumptech.glide.request.transition.Transition;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.ByteArrayOutputStream;
 
 public class user_settings extends Fragment {
 
-    TextView username;
+    private static final int GALLERY = 1, CAMERA = 2;
+    private TextView username;
+    private FirebaseFirestore firestore;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_user_settings, container, false);
 
+        firestore = FirebaseFirestore.getInstance();
+
         ImageButton profileImage = view.findViewById(R.id.profileImage);
         Glide.with(this)
                 .asBitmap()
                 .load(R.drawable.home)
                 .apply(RequestOptions.circleCropTransform())
-                .into(new CustomTarget<Bitmap>() {
-                    @Override
-                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
-                        profileImage.setImageBitmap(resource);
-                    }
-                    @Override
-                    public void onLoadCleared(@Nullable Drawable placeholder) {
-                        profileImage.setImageDrawable(placeholder);
-                    }
-                });
+                .into(profileImage);
+
+        profileImage.setOnClickListener(v -> showPictureDialog());
+
         username = view.findViewById(R.id.userName);
 
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-
         if (currentUser != null && currentUser.getDisplayName() != null) {
             username.setText(currentUser.getDisplayName());
         } else {
@@ -63,37 +58,26 @@ public class user_settings extends Fragment {
 
         return view;
     }
-    private static final String IMAGE_DIRECTORY = "/demonuts";
-    private int GALLERY = 1, CAMERA = 2;
-    final int REQUEST_CODE_GALLERY = 999;
-    private void showPictureDialog(){
-        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(getContext()); // Use getContext() instead of 'this'
+
+    private void showPictureDialog() {
+        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(getContext());
         pictureDialog.setTitle("נא לבחור מאיפה להוסיף תמונה:");
         String[] pictureDialogItems = {
                 "מהגלריה",
                 "מהמצלמה" };
-        pictureDialog.setItems(pictureDialogItems,
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        switch (which) {
-                            case 0:
-                                choosePhotoFromGallary();
-                                break;
-                            case 1:
-                                takePhotoFromCamera();
-                                break;
-                        }
-                    }
-                });
+        pictureDialog.setItems(pictureDialogItems, (dialog, which) -> {
+            if (which == 0) {
+                choosePhotoFromGallery();
+            } else if (which == 1) {
+                takePhotoFromCamera();
+            }
+        });
         pictureDialog.show();
     }
 
-    public void choosePhotoFromGallary() {
+    public void choosePhotoFromGallery() {
         Intent galleryIntent = new Intent(Intent.ACTION_PICK,
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-
         startActivityForResult(galleryIntent, GALLERY);
     }
 
@@ -102,8 +86,40 @@ public class user_settings extends Fragment {
         startActivityForResult(intent, CAMERA);
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (resultCode == getActivity().RESULT_OK && data != null) {
+            if (requestCode == GALLERY) {
+                Bitmap selectedImage = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(data.getData()));
+                String encodedImage = encodeImageToBase64(selectedImage);
+                saveImageToFirestore(encodedImage);
+            } else if (requestCode == CAMERA) {
+                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                String encodedImage = encodeImageToBase64(photo);
+                saveImageToFirestore(encodedImage);
+            }
+        }
+    }
 
+    private String encodeImageToBase64(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(byteArray, Base64.DEFAULT);
+    }
 
-
+    private void saveImageToFirestore(String encodedImage) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String userId = user.getUid();
+            firestore.collection("users").document(userId)
+                    .update("profileImage", encodedImage)
+                    .addOnSuccessListener(aVoid -> {
+                    })
+                    .addOnFailureListener(e -> {
+                    });
+        }
+    }
 }
